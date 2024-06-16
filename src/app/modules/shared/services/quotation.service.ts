8cd5e0d9909909a0fac97ea3ea1ba468e94f5f66
Laurore83 <laurore83@yahoo.fr>
2024-06-16 22:35:16 +0200

    quotation service

--- src/app/modules/shared/services/quotation.service.ts
+++ src/app/modules/shared/services/quotation.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Quotation } from '../models/quotation.classe';
+import { environment } from '../../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,4 +10,19 @@ import { Quotation } from '../models/quotation.classe';
 export class QuotationService {
   quotation!: Quotation;
   quotations!: Quotation[];
+  private readonly _BASE_URL = `${environment.apiUrl}/joke`;
+
+  constructor(private _httpClient: HttpClient) {}
+
+  getQuotationList$(): Observable<Quotation[]> {
+    return this._httpClient
+      .get<Quotation[]>(this._BASE_URL)
+      .pipe(map((response: Quotation[]) => response));
+  }
+
+  getQuotationById$(id: number): Observable<Quotation> {
+    return this._httpClient
+      .get<Quotation>(`${this._BASE_URL}/${id}`)
+      .pipe(map((Quotation: Quotation) => Quotation));
+  }
 }

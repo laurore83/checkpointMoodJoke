1efc6826d76f06f9c975ec92048e39793fd4de0e
Laurore83 <laurore83@yahoo.fr>
2024-06-16 22:06:04 +0200

    classes done

---
+++ src/app/modules/shared/models/mood.classe.ts
@@ -0,0 +1,11 @@
+export class Mood {
+  id: number;
+  name: string;
+  img: string;
+
+  constructor(id: number, name: string, img: string) {
+    this.id = id;
+    this.name = name;
+    this.img = img;
+  }
+}

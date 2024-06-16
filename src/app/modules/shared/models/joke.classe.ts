95d26b10d770848b171d92ee71f37c02fbff51ea
Laurore83 <laurore83@yahoo.fr>
2024-06-16 23:22:33 +0200

    connection with backend quotation run

--- src/app/modules/shared/models/joke.classe.ts
+++ src/app/modules/shared/models/joke.classe.ts
@@ -1,9 +1,11 @@
 export class Joke {
   id: number;
   text: string;
+  name: string;
 
-  constructor(id: number, text: string) {
+  constructor(id: number, text: string, name: string) {
     this.id = id;
     this.text = text;
+    this.name = name;
   }
 }
--- src/app/modules/shared/services/mood.service.ts
+++ src/app/modules/shared/services/mood.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Mood } from '../models/mood.classe';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,6 +10,19 @@ import { HttpClient } from '@angular/common/http';
 export class MoodService {
   Mood!: Mood;
   Moods!: Mood[];
+  private readonly _BASE_URL = `${environment.apiUrl}/mood`;
 
   constructor(private _httpClient: HttpClient) {}
+
+  getMoodList$(): Observable<Mood[]> {
+    return this._httpClient
+      .get<Mood[]>(this._BASE_URL)
+      .pipe(map((response: Mood[]) => response));
+  }
+
+  getMoodById$(id: number): Observable<Mood> {
+    return this._httpClient
+      .get<Mood>(`${this._BASE_URL}/${id}`)
+      .pipe(map((Mood: Mood) => Mood));
+  }
 }

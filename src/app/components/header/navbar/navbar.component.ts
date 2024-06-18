44db15bf1c7f6f6e16732f920fb7e3a22d3f5dde
Laurore83 <laurore83@yahoo.fr>
2024-06-18 10:35:29 +0200

    dark mode done

--- src/app/components/header/navbar/navbar.component.ts
+++ src/app/components/header/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -6,9 +6,11 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   items: MenuItem[] = [];
   checked: boolean = false; // Variable pour l'état du switch
+
+  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     this.items = [
@@ -23,10 +25,29 @@ export class NavbarComponent {
         routerLink: '/mood',
       },
     ];
+
+    // Vérifiez le mode initial et appliquez-le
+    const initialMode = localStorage.getItem('theme') || 'dark';
+    this.checked = initialMode === 'light';
+    console.log('Initial mode:', initialMode);
+    this.setTheme(this.checked);
   }
 
   onSwitchChange(event: any) {
     console.log('Switch state changed:', event.checked);
-    // Ajoutez votre logique ici pour gérer le changement de l'état du switch
+    this.setTheme(event.checked);
+  }
+
+  setTheme(isDarkMode: boolean) {
+    console.log('Setting theme to:', isDarkMode ? 'dark' : 'light');
+    if (isDarkMode) {
+      this.renderer.removeClass(document.body, 'light-mode');
+      this.renderer.addClass(document.body, 'dark-mode');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark-mode');
+      this.renderer.addClass(document.body, 'light-mode');
+      localStorage.setItem('theme', 'light');
+    }
   }
 }
